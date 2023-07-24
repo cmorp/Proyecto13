961d9958043421280a70acaa26ca7e291e54d6ca
Camila Moreno <cdmorenopino@gmail.com>
2023-07-23 20:20:41 -0400

    Corrección script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,11 @@
 
-// 3.1
+let ele1 = document.querySelector("#ele1");
 
-ele1 = document.querySelector("#ele1")
-
-ele1.addEventListener("click", function () {
-    ele = document.querySelector("#ele1");
-    ele.style.backgroundColor = "yellow";
-})
-
-// 3.2 
-
-paint = function (color) {
-    let ele2 = document.querySelector("#ele2");
-    ele2.style.backgroundColor = color;
+paint = function (color="green") {
+ele1.style.backgroundColor = color;
 };
 
-paint("green");
-ele2.addEventListener("click", function () {
-    paint("yellow")
+paint();
+ele1.addEventListener("click", function () {
+paint("yellow")
 });
